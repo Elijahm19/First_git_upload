6492cb9f80ea65d8321ce8b92e6ad8f34e7a0b59
Elijah Morton <Elijahm.kl15@gmail.com>
2021-09-03 18:21:24 -0400

    my python problem

---
+++ Projects/sum.py
@@ -0,0 +1,9 @@
+print("Calculate the sum from 1 to 100")
+i=1
+sum = 0
+while i<=100:
+    sum=sum+i
+    print(i," iteration sum is :",sum)
+    i=i+1
+print("sum of first to 100 numbers is : ",sum)
+print("End of Program")
